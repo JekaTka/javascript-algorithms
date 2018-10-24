4371b004f772c1345983ed384bc8e88a524df641
Jenya Tkachenko <jekatka@gmail.com>
2018-10-24 15:20:55 +0300

    Hash table

---
+++ src/data-structures/hash-table/HashTable.js
@@ -0,0 +1,67 @@
+import LinkedList from '../linked-list/LinkedList';
+
+const defaultHashTableSize = 32;
+
+export default class HashTable {
+
+  constructor(hashTableSize = defaultHashTableSize) {
+    // Create hash table of certain size and fill each bucket with empty linked list.
+    this.buckets = Array(hashTableSize).fill(null).map(() => new LinkedList());
+
+    // Just to keep track of all actual keys in a fast way.
+    this.keys = {};
+  }
+
+  hash(key) {
+    const hash = Array.from(key).reduce(
+      (hashAccumulator, keySymbol) => (hashAccumulator + keySymbol.charCodeAt(0)),
+      0,
+    );
+
+    // Reduce hash number so it would fit hash table size.
+    return hash % this.buckets.length;
+  }
+
+  set(key, value) {
+    const keyHash = this.hash(key);
+    this.keys[key] = keyHash;
+    const bucketLinkedList = this.buckets[keyHash];
+    const node = bucketLinkedList.find({ callback: nodeValue => nodeValue.key === key });
+
+    if (!node) {
+      // Insert new node.
+      bucketLinkedList.append({ key, value });
+    } else {
+      // Update value of existing node.
+      node.value.value = value;
+    }
+  }
+
+  delete(key) {
+    const keyHash = this.hash(key);
+    delete this.keys[key];
+    const bucketLinkedList = this.buckets[keyHash];
+    const node = bucketLinkedList.find({ callback: nodeValue => nodeValue.key === key });
+
+    if (node) {
+      return bucketLinkedList.delete(node.value);
+    }
+
+    return null;
+  }
+
+  get(key) {
+    const bucketLinkedList = this.buckets[this.hash(key)];
+    const node = bucketLinkedList.find({ callback: nodeValue => nodeValue.key === key });
+
+    return node ? node.value.value : undefined;
+  }
+
+  has(key) {
+    return Object.hasOwnProperty.call(this.keys, key);
+  }
+
+  getKeys() {
+    return Object.keys(this.keys);
+  }
+}
